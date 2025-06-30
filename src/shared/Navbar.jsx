4ff98a9f472c096842d417a78a15a6727d49e8be
ImTariquL <imtariqul21@gmail.com>
2025-06-30 21:39:45 +0600

    update

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     </>
     return (
         <div className=" bg-base-100 shadow-sm ">
-            <div className="navbar max-w-7xl mx-auto">
+            <div className="navbar w-11/12 mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
